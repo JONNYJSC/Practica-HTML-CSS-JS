dd5c6071877406aa52b5d2b23049ad5b6cbb9bfa
JONNYJSC <jjonath21@gmail.com>
2022-04-27 13:11:21 -0600

    firts commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/* ===== SHOW SCROLL TOP =====*/
+function scrollTop() {
+	const scrollTop = document.getElementById('scroll-top');
+
+	if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+	else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
